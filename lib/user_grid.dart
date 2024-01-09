76bae6c4cabcfb1b608adb2d19ac529770c3ad50
Alden Sprackling <aldensprackling@Aldens-MacBook-Air.local>
2024-01-09 10:28:03 -0600

    added firebase core, auth, and firestore

--- lib/user_grid.dart
+++ lib/user_grid.dart
@@ -21,41 +21,7 @@ class _UserGridState extends State<UserGrid> {
             mainAxisSpacing: 10,
             crossAxisCount: 2,
             children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[100],
-                child: const Text("He'd have you all unravel at the"),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[200],
-                child: const Text('Heed not the rabble'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[300],
-                child: const Text('Sound of screams but the'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[400],
-                child: const Text('Who scream'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[500],
-                child: const Text('Revolution is coming...'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[600],
-                child: const Text('Revolution, they...'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.green[600],
-                child: const Text('Revolution, they...'),
-              ),
+              // TODO: add user widgets here
             ],
           ),
         ),
